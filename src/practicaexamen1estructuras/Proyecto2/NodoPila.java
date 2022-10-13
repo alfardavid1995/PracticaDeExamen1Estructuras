c11c48e83155d1436792a85fdc3b173d3f573a50
David Alfaro <alfardavid07@gmail.com>
2022-10-12 18:59:53 -0600

    clases aparte

---
+++ src/practicaexamen1estructuras/Proyecto2/NodoPila.java
@@ -0,0 +1,29 @@
+package practicaexamen1estructuras.Proyecto2;
+
+public class NodoPila {
+    private Traste trasteNodo;
+    private NodoPila siguiente;
+    
+    //constructor para inicializar
+
+    public NodoPila() {
+        this.trasteNodo = null;
+        this.siguiente = null;
+    }
+
+    public Traste getTrasteNodo() {
+        return trasteNodo;
+    }
+
+    public void setTrasteNodo(Traste trastecitoNodo) {
+        this.trasteNodo = trastecitoNodo;
+    }
+
+    public NodoPila getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(NodoPila siguiente) {
+        this.siguiente = siguiente;
+    }
+}
